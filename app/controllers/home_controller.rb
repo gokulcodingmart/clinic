e72b5bb6787d9eae8065ec2372eaa73314064004
gokulcodingmart <gokul@codingmart.com>
2018-10-12 00:09:50 +0530

    design change

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,8 +10,18 @@ class HomeController < ApplicationController
 
   	@doctor = params[:doctorid]
     @patient = params[:patientid]
-
-    @book = Booking.create :doctor_id => @doctor, :patient_id => @patient
+    @date = params[:date]
+    @book = Booking.create :doctor_id => @doctor, :patient_id => @patient, :date => @date
+    respond_to do |format|
+      if @book  
+          
+          format.html { redirect_to root_path, notice: 'Appointment was sucessfully booked.' }
+          format.json { render :show, status: :created, location: root_path }
+        else
+          format.html { render :new }
+          format.json { render json: @pet.errors, status: :unprocessable_entity }
+        end
+    end
 
    end
 end
